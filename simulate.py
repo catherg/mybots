b35b19631edaf14226793bc47f4c1e9a981e0406
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-08 14:37:38 -0600

    Update simulate.py

--- simulate.py
+++ simulate.py
@@ -1,5 +1,9 @@
 import pybullet as p
+import time
 physicsClient = p.connect(p.GUI)
+sleep = 1/60
 for i in range(0, 1000):
     p.stepSimulation()
+    time.sleep(sleep)
+    print(i)
 p.disconnect()
