6dce4d1180b3d91afda981ba1a47e70766f36d0b
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-09 14:57:19 -0600

    updated box

--- simulate.py
+++ simulate.py
@@ -1,8 +1,12 @@
 import pybullet as p
+import pybullet_data
 import time
 physicsClient = p.connect(p.GUI)
-p.setGravity(0,0,-9.8)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 sleep = 1/60
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("box.sdf")
 for i in range(0, 1000):
     p.stepSimulation()
     time.sleep(sleep)
