e8a6e7b8d0213389116a70449d2f159b7d4f451f
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-13 11:47:27 -0600

    changed

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time
+import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 sleep = 1/60
@@ -10,6 +11,7 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(10000)
 for i in range(0, 1000):
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
