5f121e945c8af0ef968cd316eb50d35a0c46b2aa
catherg <116319364+catherg@users.noreply.github.com>
2023-01-15 14:35:11 -0600

    Delete analyze.py

--- analyze.py
+++
@@ -1,6 +0,0 @@
-import numpy
-import matplotlib.pyplot
-backLegSensorValues = numpy.load(data/backLegSensorValues.npy)
-print(backLegSensorValues)
-plot(backLegSensorValues)
-matplotlib.pyplot.show()
