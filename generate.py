c1805a08c4dd5b3afce3434bcf072cbe29f562cd
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-15 15:27:10 -0600

    changed

--- generate.py
+++ generate.py
@@ -1,27 +1,44 @@
 import pyrosim.pyrosim as pyrosim
-pyrosim.Start_SDF("boxes.sdf")
+
 length = 1
 width = 1
 height = 1
-x = -1
-y = -1
+x = 0
+y = 0
 z = 0.5
 
-for k in range(0,5):
-    z = 0.5
-    length = 1
-    width = 1
-    height = 1
-    for j in range(0,5):
-        z = 0.5
-        length = 1
-        width = 1
-        height = 1
-        for i in range(0,10):
-            length = 0.9 * length
-            width = 0.9 * width
-            height = 0.9 * height
-            pyrosim.Send_Cube(name="Box", pos=[k,j,z] , size=[length,width,height])
-            z = z + height
-    
-pyrosim.End()
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2,-2,z] , size=[length,width,height])
+    pyrosim.End()
+
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="Backleg", pos=[0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="Frontleg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.End()
+
+
+Create_World()
+Create_Robot()
+
+
+# for k in range(0,5):
+#     z = 0.5
+#     length = 1
+#     width = 1
+#     height = 1
+#     for j in range(0,5):
+#         z = 0.5
+#         length = 1
+#         width = 1
+#         height = 1
+#         for i in range(0,10):
+#             length = 0.9 * length
+#             width = 0.9 * width
+#             height = 0.9 * height
+#             pyrosim.Send_Cube(name="Box", pos=[k,j,z] , size=[length,width,height])
+#             z = z + height
--- simulate.py
+++ simulate.py
@@ -1,14 +1,22 @@
+import pyrosim.pyrosim as pyrosim
 import pybullet as p
 import pybullet_data
 import time
+import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 sleep = 1/60
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
-p.loadSDF("boxes.sdf")
+robotId = p.loadURDF("body.urdf")
+p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(10000)
 for i in range(0, 1000):
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
+    print(backLegSensorValues)
     p.stepSimulation()
+    ##backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("Backleg")
     time.sleep(sleep)
-    print(i)
+numpy.save(datas.npy, backLegSensorValues)
 p.disconnect()
