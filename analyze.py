3b77ec23e72d8494d8a325fb5723dcfdad97e0b4
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-15 19:38:17 -0600

    changes

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 import numpy
 import matplotlib.pyplot
 backLegSensorValues = numpy.load('data/backLegSensor.npy', allow_pickle = True)
-print(backLegSensorValues)
-#plot(backLegSensorValues)
-#matplotlib.pyplot.show()
+frontLegSensorValues = numpy.load('data/frontLegSensor.npy', allow_pickle = True)
+matplotlib.pyplot.plot(backLegSensorValues, label = "back leg", linewidth = '5')
+matplotlib.pyplot.plot(frontLegSensorValues, label = "front leg")
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
