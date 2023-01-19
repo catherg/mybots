a783cc8a9a39a7d3b5d7f215cecc3bfaf24ed296
Catherine Guo <CatherineGuo2025@u.northwestern.edu>
2023-01-19 14:34:39 -0600

    changed

--- generate.py
+++ generate.py
@@ -20,8 +20,19 @@ def Create_Robot():
     pyrosim.Send_Joint(name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [1,0,1])
     pyrosim.Send_Cube(name="Frontleg", pos=[-0.5,0,-0.5] , size=[length,width,height])
     pyrosim.End()
-def Generate_Body():
 
+
+def Generate_Body():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[-2,-2,z] , size=[length,width,height])
+    pyrosim.End()
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_Backleg" , parent= "Torso" , child = "Backleg" , type = "revolute", position = [2,0,1])
+    pyrosim.Send_Cube(name="Backleg", pos=[0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.Send_Joint(name = "Torso_Frontleg" , parent= "Torso" , child = "Frontleg" , type = "revolute", position = [1,0,1])
+    pyrosim.Send_Cube(name="Frontleg", pos=[-0.5,0,-0.5] , size=[length,width,height])
+    pyrosim.End()
 
 
 Create_World()
