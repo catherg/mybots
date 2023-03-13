e199d12ec9cbeb0d47e120dda59ef266256de26a
catherg <CatherineGuo2025@u.northwestern.edu>
2023-03-13 16:40:37 -0500

    changed

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ import numpy
 from hillclimber import HILLCLIMBER
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
 phc = PARALLEL_HILL_CLIMBER()
-numpy.random.seed(2)
+numpy.random.seed(3)
 phc.Evolve()
 print("GENERATIONS ALL DONE!")
 input("Press Enter to Continue")
